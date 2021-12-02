9eb8e68191b440a24b0931d62a0fea9fef2b5b04
jprice <jprice@izeni.com>
2021-12-02 16:28:48 -0700

    Closer approximation to designs on create-passcode page.

--- src/app/views/auth/create-passcode/create-passcode.jsx
+++ src/app/views/auth/create-passcode/create-passcode.jsx
@@ -1,6 +1,7 @@
 import m from 'mithril';
 import { Button, TextField } from 'polythene-mithril';
 import { Container } from '../../../components';
+import './create-passcode.scss';
 
 class CreatePasscode {
   constructor() {}
@@ -8,33 +9,40 @@ class CreatePasscode {
   view() {
     return (
       <>
-        <Container class="headspace">
-          <div>
+        <Container class="headspace flex flex-align-center">
+          <div class="flex-2">
             <img src="https://via.placeholder.com/540x440" />
           </div>
-          <div>
+          <div class="flex-3" style="padding: 1rem">
             <h1>Create a Passcode</h1>
-            <p>
+            <p class="font-color--light">
               Generate your passcode here to encrypt your desktop software and then copy into the following screen. Make
               sure to store your password somewhere safe: either in 1Password, Last Pass, a comparable app, or printed
               and in a safe deposit box.
             </p>
-            <TextField placeholder="Passcode" />
-            <Button raised label="Generate New" />
-            <br />
-            <Button label="Use 1Password" />
-            <Button label="Use Last Pass" />
-            <br />
-            <Button
-              raised
-              className="button__blue"
-              label="Continue"
-              events={{
-                onclick: () => {
-                  m.route.set('/auth/enter-passcode');
-                },
-              }}
-            />
+            <div class="passcodeInput">
+              <div class="passcode">
+                <span>OG8jvw9bTmUd5J92iKYmfU</span>
+                <i class="fas fa-copy"></i>
+              </div>
+              <Button raised label="Generate New" />
+            </div>
+            <div class="flex flex-justify-between" style={{ marginTop: '2rem' }}>
+              <Button label="Use 1Password" />
+              <Button label="Use Last Pass" />
+            </div>
+            <div class="flex flex-justify-end" style={{ marginTop: '2rem' }}>
+              <Button
+                raised
+                className="button__blue"
+                label="Continue"
+                events={{
+                  onclick: () => {
+                    m.route.set('/auth/enter-passcode');
+                  },
+                }}
+              />
+            </div>
           </div>
         </Container>
       </>
